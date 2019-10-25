9b00fe82384e01258fc8c2e94dda393fa5bba5de
tomek <GHJbnm123>
2019-10-25 12:17:37 +0200

    test

--- src/main/java/pl/cloud/test/controller/TestController.java
+++ src/main/java/pl/cloud/test/controller/TestController.java
@@ -3,22 +3,27 @@ package pl.cloud.test.controller;
 import org.hibernate.validator.internal.metadata.raw.BeanConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.cloud.test.coonfig.Configuration;
 
 @RestController
+@RefreshScope
 public class TestController {
 
     @Autowired
     private Configuration cOnfiguration;
 
+    @Value("${valores.valor_fijo}")
+    private String valores;
+
     @GetMapping("/message")
     public String mesagage() {
         System.err.println(cOnfiguration);
+        System.err.println(valores);
         return "aaaaa";
     }
-
 
 
 }
