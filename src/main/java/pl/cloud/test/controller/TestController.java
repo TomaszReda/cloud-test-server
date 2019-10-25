f59744d8e471dfb5de1438c3e73dfb00433248d1
tomek <GHJbnm123>
2019-10-25 11:50:25 +0200

    test

--- src/main/java/pl/cloud/test/controller/TestController.java
+++ src/main/java/pl/cloud/test/controller/TestController.java
@@ -1,6 +1,8 @@
 package pl.cloud.test.controller;
 
+import org.hibernate.validator.internal.metadata.raw.BeanConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.cloud.test.coonfig.Configuration;
@@ -11,10 +13,16 @@ public class TestController {
     @Autowired
     private Configuration cOnfiguration;
 
+
+    @Value("${valores.valor_fijo}")
+    String valorFijo;
+
     @GetMapping("/message")
     public String mesagage() {
         System.err.println(cOnfiguration.getMaximum());
         return "aaaaa";
     }
+
+
 
 }
